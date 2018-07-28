ec9d90fea698987aa1624637993475de2b985d6a
gunturkh <guntur.kh@gmail.com>
2018-07-28 15:44:07 +0700

    Adding append div after submit

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 $(function () {
     const taskForm = $("#task-form")
-    const resultForm = $("#result-form-1")
+    const resultField = $("#result-field")
 
 
 
-const addDOM = (event) => {
-    event.preventDefault()
-}
-    
+    const addDOM = (event) => {
+        event.preventDefault()
+
+        const divChild = document.createElement('div')
+        $('#result-field').append(`<div class="input-group mb-3" id="task-result">
+        <div class="input-group-prepend">
+            <div class="input-group-text">
+                <input type="checkbox" aria-label="Checkbox for following text input">
+            </div>
+        </div>
+        <output id="task-output" type="text" class="form-control" aria-label="Task output with checkbox">
+    </div>
+        `)
+    }
+
+
 
 
 
@@ -15,7 +27,7 @@ const addDOM = (event) => {
 
     // Event Listener
     taskForm.on("submit", addDOM)
-    
+
 
 })
 
