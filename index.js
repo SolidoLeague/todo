bfa4458201acb9a91647b3ad1d39ee199d5c16ad
gunturkh <guntur.kh@gmail.com>
2018-07-28 15:13:07 +0700

    Add font Sacramento in CSS

---
+++ index.js
@@ -0,0 +1,21 @@
+$(function () {
+    const taskForm = $("#task-form")
+    const resultForm = $("#result-form-1")
+
+
+
+const addDOM = (event) => {
+    event.preventDefault()
+}
+    
+
+
+
+
+
+    // Event Listener
+    taskForm.on("submit", addDOM)
+    
+
+})
+
