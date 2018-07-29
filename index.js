17f62c74de42da6fe5b99d03ea097916460c37b3
M. Alif Raher <alifraher@gmail.com>
2018-07-29 20:12:48 +0700

    Add attribute row and col on result-field

--- index.js
+++ index.js
@@ -1,49 +1,49 @@
 $(function () {
-    const taskForm = $("#task-form")
-    const resultField = $("#result-field")
+    const taskForm = $("#task-form");
+    const resultField = $("#result-field");
+    let i = 1;
 
-
-
-    const addDOM = (event) => {
-        event.preventDefault()
+    const addToDOM = (event) => {
+        event.preventDefault();
 
         const addTask = $("#task").val();
         const prioritySelect = $("#inputGroupSelect01").val();
         const datePicker = $("#datepicker").val();
         console.log(addTask, prioritySelect, datePicker)
 
-        if (prioritySelect == "high") 
-            $('#task-output').css('background-color', 'red');
+        if (prioritySelect == "high")
+            $('#task-output' + i).css('background-color', 'red');
         else if (prioritySelect == "med")
-            $('#task-output').css('background-color', 'yellow');
+            $('#task-output' + i).css('background-color', 'yellow');
         else if (prioritySelect == "low")
-            $('#task-output').css('background-color', 'green');
+            $('#task-output' + i).css('background-color', 'green');
 
-        $('#result-field').append(`<div class="input-group mb-3" id="task-result">
-        <div class="input-group-prepend">
-            <div class="input-group-text">
-                <input type="checkbox" aria-label="Checkbox for following text input">
+        $('#result-field').append(`
+        <div class="row justify-content-md-center">
+            <div class="col col-lg-5">
+                <div class="input-group mb-3" id="task-result">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text">
+                            <input type="checkbox" aria-label="Checkbox for following text input">
+                        </div>
+                    </div>
+                    <output id="task-output${i}" type="text" class="form-control" aria-label="Task output with checkbox">
+                        <div>
+                            <h3>${addTask}</h3>
+                        </div>
+                        <div>
+                            <h3>${datePicker}</h3>
+                        </div>
+                    </output>
+                </div>
+            </div>
+            <div class="add">
+                <button type="button" id="delete" class="btn btn-danger">x</button>
             </div>
         </div>
-        <output id="task-output" type="text" class="form-control" aria-label="Task output with checkbox">
-            <div>
-                <h3>${addTask}</h3>
-            </div>
-            <div>
-                <h3>${datePicker}</h3>
-            </div>
-        </output>
-    </div>
         `)
     }
 
-
-
-
-
-
     // Event Listener
-    taskForm.on("submit", addDOM)
-
-
-})
+    taskForm.on("submit", addToDOM);
+});
