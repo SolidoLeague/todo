c800b89a0dbaf297d6f08b6bdb67b57b2fc21303
M. Alif Raher <alifraher@gmail.com>
2018-08-06 12:52:59 +0700

    Change variable date to deadline

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 $(function () {
+
+    // Configure Datepicker
     const getDatePicker = () => {
         $('#datepicker').datepicker({
             format: 'dd/mm/yyyy',
@@ -6,6 +8,7 @@ $(function () {
         });
     }
 
+    // Show Current Date on navbar
     const getCurrentDate = () => {
         let today = new Date();
         let date = today.getDate();
@@ -20,6 +23,7 @@ $(function () {
         $('#current-date').html(today);
     }
 
+    // Disable add button when task is empty
     const checkInput = () => {
         $('#add').prop('disabled', true);
         $('#task').keyup(function () {
@@ -28,44 +32,50 @@ $(function () {
         });
     }
 
-    const getIncrement = () => {
-        const increment = Number(localStorage.getItem("increment"));
-        if (increment) return increment;
-        else return 0;
-    };
+    // Get increment from localstorage
+    //const getIncrement = () => {
+    //    const increment = Number(localStorage.getItem("increment"));
+    //    if (increment) return increment;
+    //    else return 0;
+    //};
 
-    const setIncrement = () => {
-        const currentIncrement = getIncrement();
-        localStorage.setItem("increment", currentIncrement + 1);
-    };
+    // Set increment to localstorage
+    //const setIncrement = () => {
+    //    const currentIncrement = getIncrement();
+    //    localStorage.setItem("increment", currentIncrement + 1);
+    //};
 
-    const getStorage = () => {
-        const todos = localStorage.getItem("todolist");
-        if (todos) return JSON.parse(todos);
-        else return [];
-    };
+    // Get data from localstorage
+    //const getStorage = () => {
+    //    const todos = localStorage.getItem("todolist");
+    //    if (todos) return JSON.parse(todos);
+    //    else return [];
+    //};
 
-    const setStorage = todos => {
-        localStorage.setItem("todolist", JSON.stringify(todos));
-    };
+    // Set data to localstorage
+    //const setStorage = todos => {
+    //    localStorage.setItem("todolist", JSON.stringify(todos));
+    //};
 
-    const search = () => {
-        const data = getStorage();
-        $('#search').keyup(function () {
-            const result = [];
-            data.forEach((todo, index) => {
-                const search = todo.task.toLowerCase().includes($('#search').val());
-                if (search) result.push(data[index]);
-            });
-            displayToDOM(result);
-        });
-    }
+    // Search data from localstorage
+    //const search = () => {
+    //    const data = getStorage();
+    //    $('#search').keyup(function () {
+    //        const result = [];
+    //        data.forEach((todo, index) => {
+    //            const search = todo.task.toLowerCase().includes($('#search').val());
+    //            if (search) result.push(data[index]);
+    //        });
+    //        displayToDOM(result);
+    //    });
+    //}
 
+    // Template for output
     const createTemplate = ({
         id,
         task,
         priority,
-        date
+        deadline
     }) => {
         return `
         <div class="row justify-content-md-center" id="task-result-${id}">
@@ -79,7 +89,7 @@ $(function () {
                     <output type="text" class="form-control todo-content" aria-label="Task output with checkbox">
                         <div class="d-flex flex-row ">
                             <h4 class="mr-auto" id="task-output-${id}">${task}</h4>
-                            <h5>${date}</h5>
+                            <h5>${deadline}</h5>
                         </div>
                     </output>
                 </div>
@@ -96,13 +106,13 @@ $(function () {
         id,
         task,
         priority,
-        date
+        deadline
     }) => {
         const template = createTemplate({
             id,
             task,
             priority,
-            date
+            deadline
         });
         $("#result-field").append(template);
     };
@@ -113,12 +123,7 @@ $(function () {
         const todos = data;
 
         todos.forEach(todo => {
-            addToDOM({
-                id: todo.id,
-                task: todo.task,
-                priority: todo.priority,
-                date: todo.date
-            });
+            addToDOM(todo);
         });
 
         todos.forEach(todo => {
@@ -138,23 +143,25 @@ $(function () {
         });
     };
 
-    // Add data
+    // Add data to localstorage
     const addData = event => {
         event.preventDefault();
 
-        setIncrement();
-        const todos = getStorage();
+        //setIncrement();
+        //const todos = getStorage();
 
         const todo = {
-            id: getIncrement(),
+            id: 20,
             task: $("#task").val(),
             priority: $("#priority").val(),
             date: $("#datepicker").val()
         };
 
-        todos.push(todo);
-        setStorage(todos);
-        displayToDOM(getStorage());
+        //todos.push(todo);
+        //setStorage(todos);
+        postDataServer(todo);
+        //displayToDOM(getStorage());
+        getDataServer();
         clearInput();
     };
 
@@ -171,6 +178,7 @@ $(function () {
         });
     };
 
+    // Clear input after add new task
     const clearInput = () => {
         $('#task').val("");
         $('#priority').val("Priority");
@@ -179,26 +187,42 @@ $(function () {
         checkInput();
     };
 
-    const getData = () => {
-        fetch("http://localhost:3000/todos")
+    // Method GET : Get data from server 
+    const getDataServer = () => {
+        return fetch("http://localhost:3000/todos")
             .then(response => {
                 return response.json();
             })
             .then(data => {
-                displayToDOM(data.todos);
+                console.log(data);
+                displayToDOM(data);
             });
     }
 
+    const postDataServer = (data) => {
+        var url = 'http://localhost:3000/todos';
+
+        fetch(url, {
+                method: 'POST', // or 'PUT'
+                body: JSON.stringify(data), // data can be `string` or {object}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then(response => console.log('Success:', response));
+    }
 
     // Event Listener
     $("#task-form").on("submit", addData);
 
     // Calling Function
-    displayToDOM(getStorage());
+    //displayToDOM(getStorage());
     getDatePicker();
     getCurrentDate();
     checkInput();
-    search();
-    getData();
+    //search();
+
+    getDataServer();
 
 });
